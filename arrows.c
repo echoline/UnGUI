0831aeb6c9a7995335f94e33818f9a643037324b
echoline <echoline@gmail.com>
2012-02-19 18:09:25 -0800

    the ol' shift n' sort

--- arrows.c
+++ arrows.c
@@ -1,3 +1,19 @@
+/* UnGUI shell application
+ * Copyright (C) 2012 Eli Cohen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <gtk/gtk.h>
 #include <X11/Xlib.h>
 #include <cairo/cairo-xlib-xrender.h>
@@ -39,7 +55,7 @@ void arrowsupdate() {
 	cairo_rectangle(cr, 0, 0, scrwidth, scrheight);
 	cairo_fill(cr);
 
-	cairo_set_source_rgb(cr, 0.8, 0.0, 0.0);
+	cairo_set_source_rgb(cr, 0.75, 0.1, 0.1);
 	cairo_set_line_width(cr, 5.0);
 
 	iter = objects;
--- fns.h
+++ fns.h
@@ -1,1 +1,26 @@
+/* UnGUI shell application
+ * Copyright (C) 2012 Eli Cohen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+GtkWidget* new_data_window(GtkTextBuffer **, Object **);
+void closeitem(GtkWidget *__unused, gpointer);
+void execute(GtkWidget *button, gpointer program);
+void cleanup(gpointer __unused);
+void syspath(GtkWidget *button, gpointer window);
+void newdata(GtkWidget *button, gpointer __unused);
+void opendata(GtkWidget *button, gpointer __unused);
+void openmenu(GtkWidget *button, gpointer menu);
+
 void arrowsinit();
